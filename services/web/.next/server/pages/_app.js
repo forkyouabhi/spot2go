e21692b49a6c4a6e003695a6d76ebd00c1c7194c
Forkyouabhi <forkyouabhi@proton.me>
2025-09-23 17:17:52 -0400

    gitignore fixes

--- services/web/.next/server/pages/_app.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/node_modules_c2a03b04._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__7632539a._.js")
-R.m("[project]/src/pages/_app.tsx [ssr] (ecmascript)")
-module.exports=R.m("[project]/src/pages/_app.tsx [ssr] (ecmascript)").exports
--- services/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Bfv/SwMyQA2yXFADZ31mvceuiCxUmA92Nv/2s398H+g=\"\n}"
